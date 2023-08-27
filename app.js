8312f672349525d15bcd8a7ed15527492abcd55a
jkempton1 <jckkempton@gmail.com>
2023-08-27 14:16:46 -0600

    added proper titles for each page, added willtolive page and routing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signinRouter = require('./routes/sign_in');
 var signupRouter = require('./routes/sign_up');
+var willtoliveRouter = require('./routes/will_to_live');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signin', signinRouter);
 app.use('/signup', signupRouter);
+app.use('/willtolive', willtoliveRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
